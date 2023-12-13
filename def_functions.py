0ee8dde55ce7fa74ebb280d4dc528d8c01306e4c
aho0920 <aho0920@terpmail.umd.edu>
2023-12-13 15:57:31 -0500

    Added lots of comments into final project to describe what I am completing in the checklist. Lots of additions to my main loop, such as a count for exiting program Added ability to search for number in dictionary with name. Added simple exit function.
    Just formatting changes into def_functions.  Will add comments in there.

--- def_functions.py
+++ def_functions.py
@@ -1,18 +1,18 @@
 def add_contact(phonebook, name, number):
     """Add a contact to the phonebook."""
     phonebook[name] = number
-    print(f"Contact '{name}' added successfully.")
+    print(f"\nContact '{name}' added successfully.")
 
 def view_contacts(phonebook):
     """View all contacts in the phonebook."""
-    print("Contacts:")
+    print("\nContacts:")
     for name, number in phonebook.items():
-        print(f"{name}: {number}")
+        print(f"\n{name}: {number}")
 
 def search_contacts(phonebook, name):
     """Search for a contact by name."""
     if name in phonebook:
-        print(f"Contact found - {name}: {phonebook[name]}")
+        print(f"\nContact found - {name}: {phonebook[name]}")
     else:
-        print(f"Contact '{name}' not found in the phonebook.")
+        print(f"\nContact '{name}' not found in the phonebook.")
 
--- final_project.py
+++ final_project.py
@@ -1,10 +1,11 @@
 # Final Project Program 
 
-import os
-import def_functions
+import os # importing this just in case I need this.
+import def_functions # 3.17 I created a separate python file for functions I'm using within this program.  
 
-phonebook = {}
+phonebook = {} # phonebook is our dictionary that we will base our project on, we have two keys, the "name" and their "number".  
 
+count = 0 # Creating a count in case too many errors.
 while True:
     print("\nPhonebook Menu:")
     print("1. Add Contact")
@@ -14,10 +15,30 @@ while True:
 
     choice = input("Enter your choice (1-4): ")
 
+    if count >= 4: #When count is greater than or equal to 4, then the program quits with a message.
+        print("Too many invalid choices inputted, closing program.  Bye.")
+        exit()
+
     if choice == '1':
         name = input("Enter contact name: ")
         number = input("Enter contact number: ")
-        def_functions.add_contact(phonebook, name, number)
+        def_functions.add_contact(phonebook, name, number) 
+        # 5.17 Accessing the keys in a dictonary, with "name" and "number"
+        # 5.19 Able to update values / add values into the dictionary.  
 
     elif choice == '2':
         def_functions.view_contacts(phonebook)
+
+    elif choice == '3':
+        name = input("\nEnter contact name to search: ")
+        def_functions.search_contacts(phonebook, name)
+        # 5.18 We are accessing the keys, to view the value in the dictionary.  In this case, we are pairing a name to a number.  
+
+    elif choice == '4': # Simple quit function.  
+        print("\nThanks for using this phonebook program, bye.")
+        exit()
+    
+    else: # 4.9 raising an error and providing a helpful error message.  
+        count += 1
+        print("\nInvalid choice.  Enter numbers 1 through 4.")
+    
